cdcc11000a960056edb777a08240a3259f5c12d0
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2018-08-08 16:35:41 -0400

    adding front end

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:  "1001067167827-frm2kb88gcs14roji8p2e9rhtk3huk0g.apps.googleusercontent.com",
+  googleClientSecret:  "nP5iCvHWmWBTb6jg--mP8j2M",
+  mongoURI: "mongodb://anna:password1@ds147411.mlab.com:47411/emaily-dev-akb",
+  cookieKey:'dakjhdfskfdskhjfdshk',
+  stripePublishableKey: 'pk_test_FefX3lcuApbJ96znrFNdZ0Ud',
+  stripeSecretKey: 'sk_test_R5mKsgYZgadD25gFGg4gSOFq',
+  sendGridKey: "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",
+  redirectDomain: 'http://localhost:3000'
+}
